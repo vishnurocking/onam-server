b1be2f915f157ce2ca81f505ac85fb7fbd381ac3
Vishnu S <vishnuitsrocking@gmail.com>
2024-09-23 11:40:20 +0530

    Add CORS Update

--- app.ts
+++ app.ts
@@ -20,10 +20,22 @@ app.use(express.json({ limit: "50mb" }));
 // cookie parser
 app.use(cookieParser());
 
-// cors => cross origin resource sharing
+// CORS configuration
+const allowedOrigins = process.env.ORIGIN ? process.env.ORIGIN.split(",") : [];
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
